02255b7d565f43e4c4b8dcb438e76dd042f0f692
Gustavo Lotto Cardoso <gustavo.lotto29@gmail.com>
2020-08-09 16:28:30 -0300

    desafio completo

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
+    const data = new FormData();
     try {
-      // await api.post('/transactions/import', data);
+      if (uploadedFiles) {
+        data.append('file', uploadedFiles[0].file, uploadedFiles[0].name);
+
+        await api.post('/transactions/import', data);
+      }
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const fileUpload = files[0];
+
+    const fileProps: FileProps = {
+      file: fileUpload,
+      name: fileUpload.name,
+      readableSize: filesize(fileUpload.size),
+    };
+
+    setUploadedFiles([...uploadedFiles, fileProps]);
   }
 
   return (
